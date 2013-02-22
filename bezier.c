ff5ec9950a2c29ca878ec62398337b27d60ed61a
Gnuk <gnuk@mailoo.org>
2013-02-22 10:22:08 +0100

    Affichage de la route via le calcul des courbes de bezier #question 1

--- bezier.c
+++ bezier.c
@@ -2,11 +2,40 @@
 #include "geometry.h"
 #include "bezier.h"
 
+/**
+ * Question 1
+ */
 void calcule3d(point3d result, bezier_curve3d c, double t) {
-  zero3d(result);
+	int i, j, max;
+	point3d_cell tableau[c->nb_points];
+	
+	max = c->nb_points-1;
+	for(i=0; i < c->nb_points; i++){
+		tableau[i] = c->controles[i];
+	}
+	for(i=max ; i > 0 ; i--){
+		for(j=0; j < i; j++){
+			bary3d(&tableau[j], (1.0-t),  &tableau[j] ,t, &tableau[j+1]);
+		}
+	}
+	*result = tableau[0];
 }
 
+/**
+ * Question 1
+ */
 double calcule1d(bezier_curve1d c, double t) {
-  return 0.0;
+  double tableau[c->nb_points];
+  int i, j, max;
+	for(i=0; i < c->nb_points; i++){
+		tableau[i] = c->controles[i];
+	}
+	max = c->nb_points-1;
+	for(i = max ; i > 0 ; i--){
+		for(j=0; j<i; j++){
+			tableau[j] = (1.0-t) * tableau[j] + t * tableau[j+1];
+		}
+	}
+	return tableau[0];
 }
 
